a99e4705dcd67c3ade5c3fa81610301a354778f9
Ketsueki-23 <vapedro2001@gmail.com>
2024-07-10 00:38:39 +0000

    Mundo guardado 2024-07-10

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 # forge, mohist, fabric, vanilla, paper
 
 # Puedes instalar mohist después de instalar forge desde el menú de gestionar
-# Puedes instalar paper después de instalar vanilla desde el menú de gestionar
+# Puedes instalar paper después de instalar vanilla desde el menú de
 # Puedes instalar purpur después de instalar fabric desde el menú de gestionar
 
 # Regiones de ngrok
@@ -15,15 +15,7 @@
 # jp		      Japón (Tokyo)
 # sa		      Sudamérica (São Paulo)
 # us		      Estados unidos (Ohio)
-# us-cal-1	      Estados unidos (California)
-
-
-
-
-
-
-
-
+# us-cal-1	      Estados unidos (Californi
 
 
 
